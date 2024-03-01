5bb01e953a9b32bd77a84569ef049fc0c52a7466
serveredflora <150009951+serveredflora@users.noreply.github.com>
2024-03-01 13:36:02 +0100

    refactor: Use npm package for pre-made react components for heroicons

--- client/components/dropdown.jsx
+++ client/components/dropdown.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
 
 export default function Dropdown({ data, onChangeEvent }) {
   // TODO(noah): try using 'FormData' instead of this funky react state mess...
@@ -73,37 +74,9 @@ export default function Dropdown({ data, onChangeEvent }) {
 
   const OptionsIcon = ({}) => {
     if (!showOptions) {
-      // Icon 'chevron-down' from: https://heroicons.com/mini
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            fillRule="evenodd"
-            d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      );
+      return <ChevronDownIcon className="w-5 h-5" />;
     } else {
-      // Icon 'chevron-up' from: https://heroicons.com/mini
-      return (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 20 20"
-          fill="currentColor"
-          className="w-5 h-5"
-        >
-          <path
-            fillRule="evenodd"
-            d="M9.47 6.47a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 1 1-1.06 1.06L10 8.06l-3.72 3.72a.75.75 0 0 1-1.06-1.06l4.25-4.25Z"
-            clipRule="evenodd"
-          />
-        </svg>
-      );
+      return <ChevronUpIcon className="w-5 h-5" />;
     }
   };
 
