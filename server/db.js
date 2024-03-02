601a3231a89e00aa4ab903f07e0b185e2ec55d92
nuevocosas <ericdavid.biz@gmail.com>
2024-03-03 00:46:11 +0800

    refactor: login / db interaction

---
+++ server/db.js
@@ -0,0 +1,14 @@
+const knex = require('knex');
+
+// Centralized Knex connection
+const db = knex({
+  client: 'mysql',
+  connection: {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+  },
+});
+
+module.exports = db;
