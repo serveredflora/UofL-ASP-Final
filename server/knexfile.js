85a6b44c5970b1a4068ce6b4316f6bff5294ec24
serveredflora <150009951+serveredflora@users.noreply.github.com>
2024-03-09 18:12:25 +0100

    Revert "feat: Use DB_PORT entry from `.env` file"
    This reverts commit ee605c40dae7f58b2d08f00296e2cc3ba2e1b13c.

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,17 +1,16 @@
-require("dotenv").config({ path: "../.env" });
+require('dotenv').config({ path: '../.env' });
 
 module.exports = {
   development: {
-    client: "mysql",
+    client: 'mysql',
     connection: {
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
-      port: process.env.DB_PORT,
       password: process.env.DB_PASS,
       database: process.env.DB_NAME,
     },
     migrations: {
-      tableName: "knex_migrations",
-    },
-  },
+      tableName: 'knex_migrations'
+    }
+  }
 };
