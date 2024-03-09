ee605c40dae7f58b2d08f00296e2cc3ba2e1b13c
serveredflora <150009951+serveredflora@users.noreply.github.com>
2024-03-09 16:57:11 +0100

    feat: Use DB_PORT entry from `.env` file

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
-const knex = require('knex');
+const knex = require("knex");
 
 // Centralized Knex connection
 const db = knex({
-  client: 'mysql',
+  client: "mysql",
   connection: {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
+    port: process.env.DB_PORT,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
   },
